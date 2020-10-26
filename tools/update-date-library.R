6dbbba4db310b39b998af557aacffe8251f38b78
DavisVaughan <davis@rstudio.com>
2020-10-26 16:16:17 -0400

    Install into `inst/include/date` so we can use angled brackets

--- tools/update-date-library.R
+++ tools/update-date-library.R
@@ -7,7 +7,8 @@ library(glue)
 # The `tz.cpp` file that is downloaded into `src/tz.cpp` will need to be updated
 # to pass R CMD Check. All calls to `std::cerr` will have to be commented out.
 #
-# The rest of the date API is downloaded into `src/date/` and is header only.
+# The rest of the date API is downloaded into `inst/include/date/`
+# and is header only.
 
 # ------------------------------------------------------------------------------
 # cd into temp dir
@@ -29,18 +30,19 @@ system(cmd)
 dir_date <- path(dir_dest, "date")
 
 # ------------------------------------------------------------------------------
-# Update headers and tz.cpp
-# Will overwrite `src/tz.cpp` (which will need to be tweaked)
-# and `src/date/`
-
-dir_pkg_src <- here("src")
+# Update `inst/include/date/`
 
 dir_date_include_date <- path(dir_date, "include", "date")
-dir_pkg_src_date <- path(dir_pkg_src, "date")
-dir_copy(dir_date_include_date, dir_pkg_src_date, overwrite = TRUE)
+dir_pkg_inst_include_date <- here("inst", "include", "date")
+
+dir_copy(dir_date_include_date, dir_pkg_inst_include_date, overwrite = TRUE)
+
+# ------------------------------------------------------------------------------
+# Update `src/tz.cpp`
+# Important - this file will need to be tweaked!
 
 file_date_src_tz <- path(dir_date, "src", "tz.cpp")
-file_pkg_src_tz <- path(dir_src, "tz.cpp")
+file_pkg_src_tz <- path("src", "tz.cpp")
 
 file_copy(file_date_src_tz, file_pkg_src_tz, overwrite = TRUE)
 
