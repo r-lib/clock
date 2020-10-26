f1141aa8a394b09228fecfef8f64992dce43516b
DavisVaughan <davis@rstudio.com>
2020-10-26 15:31:25 -0400

    `use_c()`

--- R/civil-package.R
+++ R/civil-package.R
@@ -4,5 +4,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @useDynLib civil, .registration = TRUE
 ## usethis namespace: end
 NULL
