4ee6a4e9b0f29f9fcb8ada542328cd3ac70748fb
DavisVaughan <davis@rstudio.com>
2020-10-29 16:34:47 -0400

    Revert making `adjust_zone_*()` generic

--- R/adjust-zone.R
+++ R/adjust-zone.R
@@ -3,56 +3,17 @@ adjust_zone_retain_clock <- function(x,
                                      zone,
                                      ...,
                                      dst_resolver = default_dst_resolver()) {
-  UseMethod("adjust_zone_retain_clock")
-}
-
-#' @export
-adjust_zone_retain_clock.Date <- function(x,
-                                          zone,
-                                          ...,
-                                          dst_resolver = default_dst_resolver()) {
-  x <- to_posixct(x)
-  adjust_zone_retain_clock(x, zone, ..., dst_resolver = dst_resolver)
-}
-
-#' @export
-adjust_zone_retain_clock.POSIXct <- function(x,
-                                             zone,
-                                             ...,
-                                             dst_resolver = default_dst_resolver()) {
   check_dots_empty()
   validate_dst_resolver(dst_resolver)
-  x <- posixct_standardize(x)
+  x <- to_posixct(x)
   adjust_zone_retain_clock_cpp(x, zone, dst_resolver)
 }
 
 #' @export
-adjust_zone_retain_clock.POSIXlt <- function(x,
-                                             zone,
-                                             ...,
-                                             dst_resolver = default_dst_resolver()) {
-  x <- to_posixct(x)
-  adjust_zone_retain_clock(x, zone, ..., dst_resolver = dst_resolver)
-}
-
-# ------------------------------------------------------------------------------
-
-#' @export
 adjust_zone_retain_instant <- function(x, zone, ...) {
-  UseMethod("adjust_zone_retain_instant")
-}
-
-#' @export
-adjust_zone_retain_instant.Date <- function(x, zone, ...) {
-  x <- to_posixct(x)
-  adjust_zone_retain_instant(x, zone, ...)
-}
-
-#' @export
-adjust_zone_retain_instant.POSIXct <- function(x, zone, ...) {
   check_dots_empty()
 
-  x <- posixct_standardize(x)
+  x <- to_posixct(x)
   zone <- zone_standardize(zone)
 
   if (!zone_is_valid(zone)) {
@@ -62,10 +23,4 @@ adjust_zone_retain_instant.POSIXct <- function(x, zone, ...) {
   attr(x, "tzone") <- zone
 
   x
-}
-
-#' @export
-adjust_zone_retain_instant.POSIXlt <- function(x, zone, ...) {
-  x <- to_posixct(x)
-  adjust_zone_retain_instant(x, zone, ...)
 }
