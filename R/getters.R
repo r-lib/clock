c31557d433495704c38a19da663e31f1f612e97a
DavisVaughan <davis@rstudio.com>
2020-10-29 17:30:50 -0400

    `get_zone()` isn't generic

--- R/getters.R
+++ R/getters.R
@@ -1,22 +1,6 @@
 #' @export
 get_zone <- function(x) {
-  UseMethod("get_zone")
-}
-
-#' @export
-get_zone.Date <- function(x) {
-  "UTC"
-}
-
-#' @export
-get_zone.POSIXct <- function(x) {
+  x <- to_posixct(x)
   tzone <- get_tzone(x)
   zone_standardize(tzone)
 }
-
-#' @export
-get_zone.POSIXlt <- function(x) {
-  get_zone.POSIXct(x)
-}
-
-# ------------------------------------------------------------------------------
