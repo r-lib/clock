64fa0140f1530d9f37bf60099d5beb36a4b0a37e
DavisVaughan <davis@rstudio.com>
2020-11-10 16:15:36 -0500

    Speed up zone getter with Dates

--- R/getters.R
+++ R/getters.R
@@ -1,6 +1,10 @@
 #' @export
 get_zone <- function(x) {
-  x <- to_posixct(x)
-  tzone <- get_tzone(x)
-  zone_standardize(tzone)
+  if (is_Date(x)) {
+    "UTC"
+  } else if (is_POSIXct(x) || is_POSIXlt(x)) {
+    zone_standardize(get_tzone(x))
+  } else {
+    stop_civil_unsupported_class(x)
+  }
 }
