f1e033ba0d2efa4fb39f8afb5eac9fa8135adaae
DavisVaughan <davis@rstudio.com>
2020-11-12 13:22:12 -0500

    Flesh out local->zoned conversion for zoned-nano-datetime

--- R/zoned-convert.R
+++ R/zoned-convert.R
@@ -9,6 +9,48 @@ as_zoned_nano_datetime.default <- function(x, ...) {
 }
 
 #' @export
+as_zoned_nano_datetime.civil_local_year_month <- function(x,
+                                                          ...,
+                                                          zone = "UTC",
+                                                          dst_nonexistent = "roll-forward",
+                                                          dst_ambiguous = "earliest") {
+  x <- as_local_nano_datetime(x)
+  as_zoned_nano_datetime(x, ..., zone = zone, dst_nonexistent = dst_nonexistent, dst_ambiguous = dst_ambiguous)
+}
+
+#' @export
+as_zoned_nano_datetime.civil_local_date <- as_zoned_nano_datetime.civil_local_year_month
+
+#' @export
+as_zoned_nano_datetime.civil_local_datetime <- as_zoned_nano_datetime.civil_local_year_month
+
+#' @export
+as_zoned_nano_datetime.civil_local_nano_datetime <- function(x,
+                                                             ...,
+                                                             zone = "UTC",
+                                                             dst_nonexistent = "roll-forward",
+                                                             dst_ambiguous = "earliest") {
+  days <- field(x, "days")
+  time_of_day <- field(x, "time_of_day")
+  nanos_of_second <- field(x, "nanos_of_second")
+
+  zone <- zone_standardize(zone)
+
+  fields <- convert_nano_datetime_fields_from_local_to_zoned(
+    days = days,
+    time_of_day = time_of_day,
+    nanos_of_second = nanos_of_second,
+    zone = zone,
+    dst_nonexistent = dst_nonexistent,
+    dst_ambiguous = dst_ambiguous
+  )
+
+  names <- names(x)
+
+  new_zoned_nano_datetime_from_fields(fields, zone, names)
+}
+
+#' @export
 as_zoned_nano_datetime.Date <- function(x, ...) {
   check_dots_empty()
   x <- to_posixct(x)
@@ -44,3 +86,5 @@ as_zoned_nano_datetime.POSIXt <- function(x, ...) {
 as_zoned_nano_datetime.civil_zoned_nano_datetime <- function(x) {
   x
 }
+
+# ------------------------------------------------------------------------------
