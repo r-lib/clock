44c2ca1be2ad93c496591a46303d36b3f4dbacfc
DavisVaughan <davis@rstudio.com>
2020-12-17 10:40:53 -0500

    Fix print method of calendar types

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,6 @@
   tzdata <- system.file("tzdata", package = "civil", mustWork = TRUE)
   clock_set_install(tzdata)
 
+  vctrs::s3_register("pillar::pillar_shaft", "clock_calendar", pillar_shaft.clock_calendar)
   vctrs::s3_register("pillar::pillar_shaft", "clock_time_point", pillar_shaft.clock_time_point)
 }
