bf818ec7059e0df2f856b0faca9e6b32c56b9bca
DavisVaughan <davis@rstudio.com>
2021-01-13 21:21:07 -0500

    Reorganize getters

--- R/getters.R
+++ R/getters.R
@@ -1,95 +1,69 @@
 #' @export
-get_zone <- function(x) {
-  UseMethod("get_zone")
-}
-
-# ------------------------------------------------------------------------------
-
-#' @export
-get_offset <- function(x) {
-  UseMethod("get_offset")
-}
-
-# ------------------------------------------------------------------------------
-
-#' @export
 get_year <- function(x) {
   UseMethod("get_year")
 }
-
-# ------------------------------------------------------------------------------
 
 #' @export
 get_quarter <- function(x, ...) {
   UseMethod("get_quarter")
 }
 
-# ------------------------------------------------------------------------------
+#' @export
+get_month <- function(x) {
+  UseMethod("get_month")
+}
 
 #' @export
 get_week <- function(x) {
   UseMethod("get_week")
 }
 
-# ------------------------------------------------------------------------------
-
-#' @export
-get_index <- function(x) {
-  UseMethod("get_index")
-}
-
-# ------------------------------------------------------------------------------
-
-#' @export
-get_month <- function(x) {
-  UseMethod("get_month")
-}
-
-# ------------------------------------------------------------------------------
-
 #' @export
 get_day <- function(x) {
   UseMethod("get_day")
 }
-
-# ------------------------------------------------------------------------------
 
 #' @export
 get_hour <- function(x) {
   UseMethod("get_hour")
 }
 
-# ------------------------------------------------------------------------------
-
 #' @export
 get_minute <- function(x) {
   UseMethod("get_minute")
 }
-
-# ------------------------------------------------------------------------------
 
 #' @export
 get_second <- function(x) {
   UseMethod("get_second")
 }
 
-# ------------------------------------------------------------------------------
-
 #' @export
 get_millisecond <- function(x) {
   UseMethod("get_millisecond")
 }
-
-# ------------------------------------------------------------------------------
 
 #' @export
 get_microsecond <- function(x) {
   UseMethod("get_microsecond")
 }
 
-# ------------------------------------------------------------------------------
-
 #' @export
 get_nanosecond <- function(x) {
   UseMethod("get_nanosecond")
+}
+
+#' @export
+get_index <- function(x) {
+  UseMethod("get_index")
+}
+
+#' @export
+get_zone <- function(x) {
+  UseMethod("get_zone")
+}
+
+#' @export
+get_offset <- function(x) {
+  UseMethod("get_offset")
 }
