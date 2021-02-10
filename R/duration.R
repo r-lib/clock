07654e6686c59c0d32aa272a9c93d706d6053148
DavisVaughan <davis@rstudio.com>
2021-02-10 09:01:23 -0500

    Update duration-helper docs

--- R/duration.R
+++ R/duration.R
@@ -2,7 +2,30 @@
 #'
 #' @description
 #' These helpers construct durations of the specified precision. Durations
-#' represent units of time in a specified precision.
+#' represent units of time.
+#'
+#' Durations are separated into two categories:
+#'
+#' **Calendrical**
+#'
+#' - year
+#' - quarter
+#' - month
+#'
+#' **Chronological**
+#'
+#' - week
+#' - day
+#' - hour
+#' - minute
+#' - second
+#' - millisecond
+#' - microsecond
+#' - nanosecond
+#'
+#' Calendrical durations are generally used when manipulating calendar types,
+#' like year-month-day. Chronological durations are generally used when
+#' working with time points, like sys-time or naive-time.
 #'
 #' @section Internal Representation:
 #'
@@ -40,6 +63,13 @@
 #' A duration of 1 quarter is defined as exactly 1/4 of a year.
 #'
 #' A duration of 1 week is defined as exactly 7 days.
+#'
+#' These conversions come into play when doing operations like adding or
+#' flooring durations. Generally, you add two calendrical durations together
+#' to get a new calendrical duration, rather than adding a calendrical and
+#' a chronological duration together. The one exception is [duration_cast()],
+#' which can cast durations to any other precision, with a potential loss of
+#' information.
 #'
 #' @param n `[integer]`
 #'
