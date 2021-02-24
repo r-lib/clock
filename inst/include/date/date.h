5bcdf69d15c15ead2ef856c0d64dab4dc8c98eb5
Davis Vaughan <davis@rstudio.com>
2021-02-24 10:36:01 -0500

    Cast to `unsigned char` before calling `toupper()` (#173)
    Note that this is a change to date.h directly
    
    The standard recommends passing unsigned char to `toupper()`, as passing a char that might be negative would result in undefined behavior. This is exactly what was happening to us when using a non-English locale.

--- inst/include/date/date.h
+++ inst/include/date/date.h
@@ -4797,7 +4797,10 @@ scan_keyword(std::basic_istream<CharT, Traits>& is, FwdIter kb, FwdIter ke)
             is.setstate(std::ios::eofbit);
             break;
         }
-        auto c = static_cast<char>(toupper(ic));
+        // clock-edit-start
+        // Note: Casting to unsigned char first is recommended by `toupper()`
+        auto c = static_cast<char>(toupper(static_cast<unsigned char>(ic)));
+        // clock-edit-stop
         bool consume = false;
         // For each keyword which might match, see if the indx character is c
         // If a match if found, consume c
@@ -4810,7 +4813,9 @@ scan_keyword(std::basic_istream<CharT, Traits>& is, FwdIter kb, FwdIter ke)
         {
             if (*st == might_match)
             {
-                if (c == static_cast<char>(toupper((*ky)[indx])))
+                // clock-edit-start
+                if (c == static_cast<char>(toupper(static_cast<unsigned char>((*ky)[indx]))))
+                // clock-edit-stop
                 {
                     consume = true;
                     if (ky->size() == indx+1)
