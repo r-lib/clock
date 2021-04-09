8bc210542f659defbdeb0ea3d530272b7c01c652
DavisVaughan <davis@rstudio.com>
2021-04-09 13:39:23 -0400

    Document exported helpers

--- src/exported.cpp
+++ src/exported.cpp
@@ -4,6 +4,17 @@
 
 // -----------------------------------------------------------------------------
 
+/*
+ * Look up a time zone by name
+ *
+ * Returns a `time_zone` pointer for use in `clock_local_to_sys()`.
+ *
+ * `""` should not be passed through here. If you need to pass through the
+ * system time zone, materialize its value with `Sys.timezone()` first.
+ *
+ * Throws a `std::runtime_error()` with an informative message if the
+ * `zone_name` does not exist in the database.
+ */
 const date::time_zone*
 clock_locate_zone(const std::string& zone_name) {
   return date::locate_zone(zone_name);
@@ -11,6 +22,17 @@ clock_locate_zone(const std::string& zone_name) {
 
 // -----------------------------------------------------------------------------
 
+/*
+ * Pair a local time with a time zone to compute the corresponding sys time
+ *
+ * Returns `sys_result`, a struct with two members:
+ * - `date::sys_seconds st`: The sys time. Undefined if `!ok`.
+ * - `bool ok`: If `lt` represents a nonexistent time, then `ok = false`.
+ *
+ * Clients should check `ok` before accessing `st`.
+ *
+ * For ambiguous times, always returns the earliest of the two possible times.
+ */
 struct rclock::sys_result
 clock_local_to_sys(const date::local_seconds& lt,
                    const date::time_zone* p_time_zone) {
