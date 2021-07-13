8a93fda267af8d484dc6b3a8c893eade9dea8d7a
DavisVaughan <davis@rstudio.com>
2021-07-13 15:20:37 -0400

    Implement `invalid_remove()`

---
+++ tests/testthat/_snaps/invalid.md
@@ -0,0 +1,4 @@
+# errors on unsupported types
+
+    no applicable method for 'invalid_remove' applied to an object of class "c('double', 'numeric')"
+
---
+++ tests/testthat/test-invalid.R
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------------------------
+# invalid_remove()
+
+test_that("can remove invalid dates from calendar types", {
+  x <- year_month_day(2020, 2, 28:30)
+  expect_identical(invalid_remove(x), x[1:2])
+
+  x <- year_day(2019, 365:366)
+  expect_identical(invalid_remove(x), x[1L])
+})
+
+test_that("errors on unsupported types", {
+  expect_snapshot_error(invalid_remove(1))
+})
